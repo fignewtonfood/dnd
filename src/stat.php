2688fd0cede47603b1b0a88aa09ba0bec2270a06
Dana <student@epicodus.com>
2015-08-31 15:16:33 -0700

    second test commit

---
+++ src/stat.php
@@ -0,0 +1,134 @@
+<?php
+
+    class Stat
+    {
+        private $id;
+        private $str;
+        private $dex;
+        private $con;
+        private $intel;
+        private $wis;
+        private $cha;
+        private $init;
+        private $max_hp;
+        private $speed;
+        private $ac;
+        private $proficiency;
+        private $stats;
+
+        function setStr
+        {
+
+        }
+
+        function setDex
+        {
+
+        }
+
+        function setCon
+        {
+
+        }
+
+        function setIntel
+        {
+
+        }
+
+        function setWis
+        {
+
+        }
+
+        function setCha
+        {
+
+        }
+
+        function setInit
+        {
+
+        }
+
+        function setMax_hp
+        {
+
+        }
+
+        function setSpeed
+        {
+
+        }
+
+        function setAc
+        {
+
+        }
+
+        function setProficiency
+        {
+
+        }
+
+        function getId
+        {
+
+        }
+
+        function getStr
+        {
+
+        }
+
+        function getDex
+        {
+
+        }
+
+        function getCon
+        {
+
+        }
+
+        function getIntel
+        {
+
+        }
+
+        function getWis
+        {
+
+        }
+
+        function getCha
+        {
+
+        }
+
+        function getInit
+        {
+
+        }
+
+        function getMax_hp
+        {
+
+        }
+
+        function getSpeed
+        {
+
+        }
+
+        function getAc
+        {
+
+        }
+
+        function getProficiency
+        {
+
+        }
+    }
+ ?>

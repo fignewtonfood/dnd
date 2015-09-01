d77d2af23b62bd3465e55a035c10023d8bb29921
Ike Mattice <student@epicodus.com>
2015-09-01 14:16:15 -0700

    add database integration to class.html.twig and background.html.twig.

--- app/app.php
+++ app/app.php
@@ -27,7 +27,7 @@
     //renders homepage
     $app->get('/', function() use ($app)
     {
-        return $app['twig']->render('home.html.twig');
+        return $app['twig']->render('home.html.twig', array('characters' => Character::getAll()));
     });
 
 
@@ -39,36 +39,65 @@
     });
 
     //carry race id to class page
-    $app->post('/race', function() use ($app)
+    $app->post('/class', function() use ($app)
     {
         $race_id = $_POST['race_id'];
-        return $app['twig']->render('class.html.twig', array('race' => Race::getAll(), 'classes' => CharClass::getAll()));
+        return $app['twig']->render('class.html.twig', array('races' => Race::getAll(), 'classes' => CharClass::getAll()));
     });
 
 
 //class page
-    //renders class page
-    $app->get('/class', function() use ($app)
+    //carry race id and class id to background page
+    $app->post('/background', function() use ($app)
     {
-        return $app['twig']->render('class.html.twig', array('classes' => CharClass::getAll()));
+        $race_id = $_POST['race_id'];
+        $class_id = $_POST['class_id'];
+        return $app['twig']->render('background.html.twig', array('races' => Race::getAll(), 'classes' => CharClass::getAll(), 'backgrounds' => Background::getAll()));
     });
 
 
+
 //background page
-    //renders background page
+    //carry race id, class id, background id to stats page
+    $app->post('/stats', function() use ($app)
+    {
+        $race_id = $_POST['race_id'];
+        $class_id = $_POST['class_id'];
+        $background_id = $_POST['background_id'];
+        return $app['twig']->render('stats.html.twig', array('races' => Race::getAll(), 'classes' => CharClass::getAll(), 'backgrounds' => Background::getAll()));
+    });
 
-
-//stats page
-    //renders stat page
-
-
-//skills page
-    //renders skills page
-
-
-//summary page
-    //renders summary page
-
+// //stats page
+//     //carry race id, class id, background id, stats id to skills page
+//     $app->post('/stats', function() use ($app)
+//     {
+//         $race_id = $_POST['race_id'];
+//         $class_id = $_POST['class_id'];
+//         $background_id = $_POST['background_id'];
+//         return $app['twig']->render('stats.html.twig', array('races' => Race::getAll(), 'classes' => CharClass::getAll(), 'backgrounds' => Background::getAll()));
+//     });
+//
+//
+// //skills page
+//     //carry race id, class id, background id, stats id, skill id to skills page
+//     $app->post('/stats', function() use ($app)
+//     {
+//         $race_id = $_POST['race_id'];
+//         $class_id = $_POST['class_id'];
+//         $background_id = $_POST['background_id'];
+//         return $app['twig']->render('stats.html.twig', array('races' => Race::getAll(), 'classes' => CharClass::getAll(), 'backgrounds' => Background::getAll()));
+//     });
+//
+//
+// //summary page
+//     //carry race id, class id, background id, stats id, skill id to skills page
+//     $app->post('/stats', function() use ($app)
+//     {
+//         $race_id = $_POST['race_id'];
+//         $class_id = $_POST['class_id'];
+//         $background_id = $_POST['background_id'];
+//         return $app['twig']->render('stats.html.twig', array('races' => Race::getAll(), 'classes' => CharClass::getAll(), 'backgrounds' => Background::getAll()));
+//     });
 
 
 return $app;

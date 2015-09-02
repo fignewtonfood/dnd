d7faf057896a698fdc99ec16ee94349f224977aa
Dana <student@epicodus.com>
2015-09-02 15:44:18 -0700

    Finalize in progress

--- src/Finalize.php
+++ src/Finalize.php
@@ -70,7 +70,61 @@
 
         function save()
         {
-            
+            $GLOBALS['DB']->exec("INSERT INTO races (description, name) VALUES ('{Race::getDescription()}', '{Race::getName()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+
+            $GLOBALS['DB']->exec("INSERT INTO classes (name, description) VALUES ('{$this->getName()}', '{$this->getDescription()}')");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+
+            $GLOBALS['DB']->exec("INSERT INTO backgrounds (name, description) VALUES ('{$this->getName()}', '{$this->getDescription()}')");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+
+            $GLOBALS['DB']->exec("INSERT INTO stats (ac, acrobatics,
+                animal_handling, arcana, athletics, cha, con, deception,
+                dex, history, init, insight, intel, intimidation,
+                investigation, max_hp, medicine, nature, perception,
+                performance, persuasion, proficiency, religion, speed,
+                sleight_of_hand, stealth, str, survival, wis) VALUES
+                ({$this->getAc()}, {$this->getAcrobatics()},
+                {$this->getAnimalHandling()}, {$this->getArcana()},
+                {$this->getAthletics()}, {$this->getCha()}, {$this->getCon()},
+                {$this->getDeception()}, {$this->getDex()},
+                {$this->getHistory()}, {$this->getInit()},
+                {$this->getInsight()}, {$this->getIntel()},
+                {$this->getIntimidation()}, {$this->getInvestigation()},
+                {$this->getMaxHp()}, {$this->getMedicine()},
+                {$this->getNature()}, {$this->getPerception()},
+                {$this->getPerformance()}, {$this->getPersuasion()},
+                {$this->getProficiency()}, {$this->getReligion()},
+                {$this->getSpeed()}, {$this->getSleightOfHand()},
+                {$this->getStealth()}, {$this->getStr()},
+                {$this->getSurvival()}, {$this->getWis()});");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+
+            $GLOBALS['DB']->exec("INSERT INTO descriptions (gender, age,
+            alignment, height, eye_color, hair_color, skin_tone, other)
+            VALUES ('{$this->getGender()}', '{$this->getAge()}',
+            '{$this->getAlignment()}', '{$this->getHeight()}',
+            '{$this->getEyeColor()}', '{$this->getHairColor()}',
+            '{$this->getSkinTone()}',  '{$this->getOther()}');");
+            $this->id = $GLOBALS["DB"]->lastInsertId();
+
+            $GLOBALS['DB']->exec("INSERT INTO characters (description_id, race_id, stat_id) VALUES ({$this->getDescriptionId()}, {$this->getRaceId()}, {$this->getStatId()})");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        function deleteAll()
+        {
+
+        }
+
+        function getAll()
+        {
+            $raw_info = $GLOBALS["DB"]->query("SELECT * FROM races;");
+            $raw_info = $GLOBALS["DB"]->query("SELECT * FROM classes;");
+            $raw_info = $GLOBALS["DB"]->query("SELECT * FROM backgrounds;");
+            $raw_info = $GLOBALS["DB"]->query("SELECT * FROM stats;");
+            $raw_info = $GLOBALS["DB"]->query("SELECT * FROM characters;");
         }
     }
  ?>
